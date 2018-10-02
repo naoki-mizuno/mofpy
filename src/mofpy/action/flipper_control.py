9ea272ca5ef416cc01c5f8f9be90caf9ccb9b137
Naoki Mizuno <mizuno.naoki@rm.is.tohoku.ac.jp>
2018-10-03 07:27:41 +0900

    Add initial implementation of quince_teleop action
    Docs and refactoring TBD...

--- src/mofpy/action/flipper_control.py
+++ src/mofpy/action/flipper_control.py
@@ -2,6 +2,7 @@ import rospy
 from flipper_msgs.msg import flipper_control
 
 from .action import Action
+from ..shared import Shared
 
 
 class PubFlipperControl(Action):
@@ -18,12 +19,16 @@ class PubFlipperControl(Action):
         self.__topic = self.get_required('topic')
         self.__angle = self.get_required('angle')
 
+        if not Shared.has('__flipper_command__'):
+            Shared.add('__flipper_command__', flipper_control())
+
         self.__pub = rospy.Publisher(self.__topic,
                                      flipper_control,
                                      queue_size=1)
 
     def execute(self, named_joy=None):
-        msg = flipper_control()
+        msg = Shared.get('__flipper_command__')
+
         # TODO: Populate all values in the message
         msg.fl = self.__angle['fl']
         msg.fr = self.__angle['fr']
@@ -31,6 +36,7 @@ class PubFlipperControl(Action):
         msg.rr = self.__angle['rr']
 
         self.__pub.publish(msg)
+        Shared.add('__flipper_command__', msg)
 
 
 Action.register_preset(PubFlipperControl)
